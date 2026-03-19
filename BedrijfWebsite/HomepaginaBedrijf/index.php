200fed0d63ae080353c54f202f69509b634d17cd
ArdaUtu <9024283@student.zadkine.nl>
2026-03-19 14:02:28 +0100

    Ik heb de homepagina gemaakt

---
+++ BedrijfWebsite/HomepaginaBedrijf/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="TitelWebsite">Ontdek Creatieve Workshops</h1>
+
+    <p class="Introductie">Volg inspirerende workshops gegeven door professionele kunstenaars in heel Nederland.</p>
+
+    <button type="button" class="OverMijButton">Over mij</button>
+
+    <button type="button" class="WorkshopsButton">Neem contact</button>
+
+    <p class="Beschrijving">Bij WorkshopHub brengen we creatieve mensen samen met inspirerende workshops. Leer schilderen, illustreren en ontwerpen van professionele kunstenaars.</p>
+     
+     <h2 class="WorkshopTitel">Schilderworkshop</h2>
+     <p class ="Prijs">Vanaf €10 per persoon</p>
+     <p class ="Duur">Duur: 2 – 3 uur</p>
+     <p class ="Geschikt">Geschikt voor bedrijven en teams</p>
+     <p class ="Groepsgrootte">Groepsgrootte: 20 – 50 personen</p>
+     
+         <button type="button" class="FAQSButton">Vragen?</button>
+
+
+</body>
+</html>
