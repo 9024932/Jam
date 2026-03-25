76810ba2a100e327a7a88f3f3a1c2603521c97f5
ArdaUtu <9024283@student.zadkine.nl>
2026-03-25 10:55:09 +0100

    Update met navbar

--- BedrijfWebsite/HomepaginaBedrijf/index.php
+++ BedrijfWebsite/HomepaginaBedrijf/index.php
@@ -7,24 +7,36 @@
     <title>Document</title>
 </head>
 <body>
+
+<?php include "./Navbar.php" ?>
+
+
     <h1 class="TitelWebsite">Ontdek Creatieve Workshops</h1>
 
     <p class="Introductie">Volg inspirerende workshops gegeven door professionele kunstenaars in heel Nederland.</p>
 
     <button type="button" class="OverMijButton">Over mij</button>
 
-    <button type="button" class="WorkshopsButton">Neem contact</button>
+    <button type="Workshopbutton" class="WorkshopsButton">Neem contact</button>
 
     <p class="Beschrijving">Bij WorkshopHub brengen we creatieve mensen samen met inspirerende workshops. Leer schilderen, illustreren en ontwerpen van professionele kunstenaars.</p>
      
+
+
+    <p class="Locatie">Door heel nederland</p>
+    <p class="Begeleiding">Begeleiding door professionele kunstenaar</p>
+    <p class="Locatie">Locatie op eigen bedrijven</p>
+    <p class="Groepsgrootte">Voor bedrijven en groepen 20 - 50 personen</p>
+
+
      <h2 class="WorkshopTitel">Schilderworkshop</h2>
      <p class ="Prijs">Vanaf €10 per persoon</p>
      <p class ="Duur">Duur: 2 – 3 uur</p>
      <p class ="Geschikt">Geschikt voor bedrijven en teams</p>
      <p class ="Groepsgrootte">Groepsgrootte: 20 – 50 personen</p>
      
-         <button type="button" class="FAQSButton">Vragen?</button>
+     <button type="button" class="FAQSButton">Vragen?</button>
 
-
+     <?php include "./Footer.php" ?>
 </body>
 </html>
