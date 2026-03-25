455c8ff76a9bea58199b11fa36db29d582b3be42
9024454 <9024454@student.zadkine.nl>
2026-03-25 13:47:21 +0100

    update contact

--- BedrijfWebsite/HomepaginaBedrijf/Navbar.php
+++ BedrijfWebsite/HomepaginaBedrijf/Navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar" aria-label="Hoofdnavigatie">
   <ul class="navbar__list">
-    <li class="navbar__item"><a class="navbar__link" href="Home.php">Home</a></li>
+    <li class="navbar__item"><a class="navbar__link" href="index.php">Home</a></li>
     <li class="navbar__item"><a class="navbar__link" href="Contact.php">Contact</a></li>
     <li class="navbar__item"><a class="navbar__link" href="OverOns.php">Over ons</a></li>
     <li class="navbar__item"><a class="navbar__link" href="FAQS.php">FAQS</a></li>
