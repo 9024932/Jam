dd4a07f238593bf911aa202d4aa16e8e4f64e78d
ArdaUtu <9024283@student.zadkine.nl>
2026-03-26 09:57:48 +0100

    Update

--- BedrijfWebsite/HomepaginaBedrijf/index.php
+++ BedrijfWebsite/HomepaginaBedrijf/index.php
@@ -1,40 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
+
 <body>
 
-<?php include "./Navbar.php" ?>
+    <?php include "./Navbar.php" ?>
 
-<section class="Section1">
-    <h1 class="TitelWebsite">Ontdek Creatieve <br> Workshops</h1>
-    <p class="Introductie">Volg inspirerende workshops gegeven door <br> professionele kunstenaars in heel Nederland.</p>
-    <button type="button" class="OverMijButton">Over mij</button>
-    <button type="Workshopbutton" class="WorkshopsButton">Neem contact</button>
-    <p class="Beschrijving">Bij WorkshopHub brengen we creatieve mensen samen met <br> inspirerende workshops. Leer schilderen, illustreren en ontwerpen van professionele kunstenaars.</p>
-</section>
+    <section class="Section1">
+        <section class="right">
+            <h1 class="TitelWebsite">Ontdek Creatieve <br> Workshops</h1>
+            <p class="Introductie">Volg inspirerende workshops gegeven door <br> professionele kunstenaars in heel Nederland.</p>
+            <div class="buttonSection">
+                <button type="button" class="OverMijButton">Over mij</button>
+                <button type="Workshopbutton" class="WorkshopsButton">Neem contact</button>
+            </div>
+            <p class="Beschrijving">Bij WorkshopHub brengen we creatieve mensen samen met <br> inspirerende workshops. Leer schilderen, illustreren en ontwerpen van professionele kunstenaars.</p>
+        </section>
 
-<section class="Section2">
-    <p class="Locatie">Door heel nederland</p>
-    <p class="Begeleiding">Begeleiding door professionele kunstenaar</p>
-    <p class="Locatie">Locatie op eigen bedrijven</p>
-    <p class="Groepsgrootte">Voor bedrijven en groepen 20 - 50 personen</p>
-</section>
+        <section class="imageHeader">
+            <img class="HeaderImage" src="./Afbeeldingen/WorkshopHeader.png" alt="Workshop Header">
+        </section>
+    </section>
 
-<section class ="Section3">
-     <h2 class="WorkshopTitel">Schilderworkshop</h2>
-     <p class ="Prijs">Vanaf €10 per persoon</p>
-     <p class ="Duur">Duur: 2 – 3 uur</p>
-     <p class ="Geschikt">Geschikt voor bedrijven en teams</p>
-     <p class ="Groepsgrootte">Groepsgrootte: 20 – 50 personen</p>
-     
-     <button type="button" class="FAQSButton">Vragen?</button>
-</section>
+    <section class="Section2">
+        <p class="Locatie">Door heel nederland</p>
+        <p class="Begeleiding">Begeleiding door professionele kunstenaar</p>
+        <p class="Locatie">Locatie op eigen bedrijven</p>
+        <p class="Groepsgrootte">Voor bedrijven en groepen 20 - 50 personen</p>
+    </section>
 
-     <?php include "./Footer.php" ?>
+    <section class="Section3">
+        <h2 class="WorkshopTitel">Schilderworkshop</h2>
+        <p class="Prijs">Vanaf €10 per persoon</p>
+        <p class="Duur">Duur: 2 – 3 uur</p>
+        <p class="Geschikt">Geschikt voor bedrijven en teams</p>
+        <p class="Groepsgrootte">Groepsgrootte: 20 – 50 personen</p>
+
+        <button type="button" class="FAQSButton">Vragen?</button>
+    </section>
+
+    <?php include "./Footer.php" ?>
 </body>
+
 </html>
