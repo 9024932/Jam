4ebc5e67be928394ee4dd5bb708d3688c120929e
ArdaUtu <9024283@student.zadkine.nl>
2026-03-26 09:10:48 +0100

    Update

--- BedrijfWebsite/HomepaginaBedrijf/Footer.php
+++ BedrijfWebsite/HomepaginaBedrijf/Footer.php
@@ -7,14 +7,9 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="d2">
-<<<<<<< HEAD
+    <div class="footer">
     <p class ="FooterText">© 2023 WorkshopHub</p> 
     <p class ="FooterLinks">Cookiebeleid Algemene voorwaarden Privacy beleid</p>
-=======
-    <p class ="FooterText">© 2026 WorkshopHub</p> 
-    <p class ="FooterLinks">Cookiebeleid &nbsp;  Algemene voorwaarden Privacy beleid</p>
->>>>>>> 707efd093fa64a2c79f5a8b8bc2b8d9325b4d2eb
     </div>
 </body>
 </html>
