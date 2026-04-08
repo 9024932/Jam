2f85b194a2fc6e4d494ec3ef1990dc71d5ba2cc6
Juliany Rosenblad <9024932@student.zadkine.nl>
2026-04-08 13:02:14 +0200

    update

--- BedrijfWebsite/HomepaginaBedrijf/Overmij.php
+++ BedrijfWebsite/HomepaginaBedrijf/Overmij.php
@@ -4,14 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Over Mij</title>
 </head>
 
 <body>
     <?php include "./Navbar.php" ?>
+        <section class="card"><h1>Hee, Hallo Ik ben Janine.</h1>
+        <p>Creativiteit heeft mij altijd geraakt. Ik merkte hoe fijn
+            het is om even uit je verstand op nul te zetten en met
+            je handen bezig te zijn. Zeker in een wereld waarin we
+            vooral achter schermen zitten.
+        </p>
 
+        <p>Wat ik mijn klanten gun?
+            Dat ze naar huis gaan met meer verbinding,
+            meer plezier en een herinnering die verder
+            gaat dan een standaard teamuitje.
+        </p></section>
 
-
+        
     <?php include "./Footer.php" ?>
 </body>
 
