cce065b4e9417f744adc2d08185521f6f4cd7aba
ArdaUtu <9024283@student.zadkine.nl>
2026-04-22 09:31:53 +0200

    Update craftly

--- BedrijfWebsite/HomepaginaBedrijf/Footer.php
+++ BedrijfWebsite/HomepaginaBedrijf/Footer.php
@@ -1,4 +1,4 @@
-<footer class="footer">
-    <p class="FooterText">© 2026 WorkshopHub</p>
+﻿<footer class="footer">
+    <p class="FooterText">© 2026 Craftly</p>
     <p class="FooterLinks">Cookiebeleid &nbsp;&nbsp;&nbsp;&nbsp; &nbsp; Algemene voorwaarden &nbsp;&nbsp;&nbsp;&nbsp; &nbsp; Privacy beleid</p>
 </footer>
